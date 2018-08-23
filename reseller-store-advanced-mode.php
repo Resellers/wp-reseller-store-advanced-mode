acfb31024c4e38614b01bab2c6e140e3d762b7e4
Bryan Focht <bxfocht@godaddy.com>
2018-08-23 15:38:32 -0700

    1.5.1

--- reseller-store-advanced-mode.php
+++ reseller-store-advanced-mode.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: GoDaddy Reseller Store Advanced Settings
  * Description: Advanced debug settings for the reseller store plugin
- * Version: 1.5.0
+ * Version: 1.5.1
  * Author: Reseller Team
  * Author URI: https://www.godaddy.com/reseller-program
  * License: GPL-2.0
@@ -37,7 +37,7 @@ final class Plugin {
 	 *
 	 * @var string
 	 */
-	const VERSION = '1.5.0';
+	const VERSION = '1.5.1';
 
 	/**
 	 * Plugin prefix.
